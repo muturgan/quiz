0f8f9e7edd4762eb3d4907dba33562f844bef53c
Andrey Sakharov <a.sakharov@biletix.ru>
2020-07-26 21:52:49 +0300

    finally im done with it

---
+++ just_route.js
@@ -0,0 +1,39 @@
+const { Router } = require('express');
+const { pool } = require('../db');
+const { InsuranceOffer } = require('../models/InsuranceOffer');
+
+const insuranceRouter = new Router();
+
+
+insuranceRouter.get('/price/:type', async (req, res) => {
+   try {
+      const userId = res.locals.userId;
+      const insuranceType = req.params.type;
+
+      const conn = await pool.getConnection();
+      const insuranceRows = await conn.query(
+         `SELECT * FROM insurance WHERE type = '${insuranceType}'`
+      );
+      const agreementsRows = await conn.query(`
+         SELECT COUNT(*) AS count FROM agreements
+         WHERE userId = ${userId} AND status = 1
+      `);
+      conn.release();
+
+      const basePrice = insuranceRows[0].price;
+      const completedAgreementsCount = agreementsRows[0].count;
+      const price = basePrice * (100 - completedAgreementsCount) / 100;
+
+      const insuranceOffer = new InsuranceOffer();
+      insuranceOffer.type = insuranceType;
+      insuranceOffer.price = price;
+
+      res.status(200).send(insuranceOffer);
+   }
+   catch (err) {
+      console.error(err);
+      res.status(500).send(err);
+   }
+});
+
+module.exports = usersRouter;
